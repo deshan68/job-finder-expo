890dab4e0c6dc00cea200dbe544cd85a941cc402
deshan68 <arundeshan@gmail.com>
2023-05-02 11:48:13 +0530

    create folder structure

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+
+import { COLORS, icons, SIZES, images } from "../constants";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
